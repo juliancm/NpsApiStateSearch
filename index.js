13844b5e64661dcbbe22b5cc867971d765f2de9b
juliancm <jc.juliancastro@gmail.com>
2020-02-20 21:24:17 -0600

    added add state dropdowns

--- index.js
+++ index.js
@@ -91,6 +91,8 @@ function constStateSearch () {
 
     inputString += `
             </select>
+            <div class='addStateArea'></div>
+            <button type="submit" class="addState">Add State</button>
           <button type="submit" class="stateSearch">Search</button>
         </fieldset>
       </form>
@@ -108,18 +110,39 @@ apiKey = 'kDkPfS91teEOvc0laDYv2erETG7K3DX8MxDxagyn'
 
 https://developer.nps.gov/api/v1/parks?stateCode=FL%2C%20GA&limit=10&api_key=kDkPfS91teEOvc0laDYv2erETG7K3DX8MxDxagyn */
 
+function addStateClick () {
+  $('.addState').on('click', function(event) {
+    event.preventDefault();
+      let inputString ="";
+
+      inputString += `
+        <label for="stateDropdown2"></label>
+          <select id="stateDropdown2">
+      `
+
+      for (let i=0;i<usStates.length;i++) {
+        inputString += `<option value="${usStates[i].abbreviation}">${usStates[i].name}</option>`
+      };
+
+      inputString += `</select>`
+
+      $('.addStateArea').append(inputString);
+    });
+}
 
 
 
-function displayHandleClick () {
+function displaySearchResultsClick () {
 
     $('.stateSearch').on('click', function(event) {
         event.preventDefault();
           let stateInput = $('#stateDropdown').val();
           let numResultsInput = $('#numResults').val();
             getJson(stateInput, numResultsInput);
+            console.log(stateInput);
     });
 }
+
 
 function getJson (stateInput, numResultsInput) {
 
@@ -155,70 +178,5 @@ function displayJson (responseJson) {
 
 
 constStateSearch();
-displayHandleClick ();
-
-/* 'use strict'
-
-function constHandleInput () {
-
-  let inputString ="";
-
-  inputString += `
-    <form>
-      <fieldset>
-      <label for="handleInput">Git Hub Handle:</label>
-        <input type="text" id="handledInput">
-        <button type="submit" class="searchSubmit">Display</button>
-      </fieldset>
-    </form>
-    <div class='handleDisplayArea'></div>
-  `
-
-  $('.container').html(inputString);
-}
-
-function displayHandleClick () {
-
-    $('.searchSubmit').on('click', function(event) {
-        event.preventDefault();
-
-        if ($('#handledInput').val() === '') {
-
-            alert('please input a handle');
-
-        } else {
-          let username = $('#handledInput').val();
-            getJson(username);
-        }
-    });
-}
-
-function getJson (username) {
-  fetch(`https://api.github.com/users/${username}/repos`)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(response.statusText);
-    })
-    .then(responseJson =>
-      displayJson(responseJson))
-    .catch(error => {
-        $('.handleDisplayArea').html(`<p>${error.message}</p>`);
-    });
-}
-
-function displayJson (responseJson) {
-  $('.handleDisplayArea').empty();
-  for (let i=0; i<responseJson.length; i++) {
-  let resultString = ""
-
-  resultString += `<p>${responseJson[i].name}</p><a href="${responseJson[i].clone_url}">${responseJson[i].clone_url}</p>`
-
-  $('.handleDisplayArea').append(resultString);
-}
-}
-
-constHandleInput();
-displayHandleClick ();
-*/
+addStateClick();
+displaySearchResultsClick ();
